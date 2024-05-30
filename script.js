027d52c40bcd0d381ab2bd2c6ee9f4921453945c
Vishal <shekha.vishal1@gmail.com>
2024-05-30 18:28:10 +0530

    seperate files created

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    document.getElementById('message').style.display = 'block';
+    document.getElementById('button').style.display = 'none';
+}
+
+function hideMessage() {
+    document.getElementById('message').style.display = 'none';
+    document.getElementById('button').style.display = 'block';
+}
